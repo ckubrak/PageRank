52341d3a3542eded4539fdb1726046081e590f0a
Cristian Kubrak <kubrakcristian@gmail.com>
2018-04-09 00:10:20 -0300

    crearMatrices listo

--- testsPropios/crearMatrices.py
+++ testsPropios/crearMatrices.py
@@ -1,18 +1,43 @@
 from random import randint
 import spam
 import sys
+import os
+import subprocess
+import time
+import matplotlib.pyplot as plt
 
 def crearMatriz(n,m):
-    file = open("test_" + str(n) + "_" + str(m) + ".txt"  ,"w")
+    file = open("tests/test_" + str(n) + "_" + str(m) + ".txt"  ,"w")
     file.write(str(n) + "\n" + str(m) + "\n")
+    froms = []
+    tos = []
     for x in range(1,m):
         desde = randint(1,m)
+        while desde in froms:
+            desde = randint(1,m)
         to = randint(1,m)
-        while(to == desde):
-            to = randint(1,m)
+        while to in tos or to == desde:
+            desde = randint(1,m)
         file.write(str(desde) + " " + str(to) + "\n")
 n = int(sys.argv[1])
 mFinal = int(sys.argv[2])
 saltos = int(sys.argv[3])
+proba = sys.argv[4]
+
+res = []
 for m in range(saltos,mFinal,saltos):
     crearMatriz(n,m)
+    archivo = os.path.join("tests/", "test_" + str(n) + "_" + str(m) + ".txt")
+    start_time = time.time()    
+    subprocess.call(["./../build/tp", archivo, proba])
+    elapsed = time.time() - start_time
+    os.remove(archivo)
+    os.remove(archivo + ".outi")
+    res.append((elapsed,m))
+
+plt.scatter(*zip(*res))
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+    
