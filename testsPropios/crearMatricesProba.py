3b6a95e8e0cddea801502092e114014f25358cd9
Cristian Kubrak <kubrakcristian@gmail.com>
2018-04-11 21:37:14 -0300

    En proba no se borran mas las matrices de salida

--- testsPropios/crearMatricesProba.py
+++ testsPropios/crearMatricesProba.py
@@ -41,7 +41,7 @@ for proba in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.99]:
     promedio = sum(tiempos)/float(len(tiempos))
     os.remove(archivo)
     os.remove(archivoRes)
-    os.remove(archivo + ".outi")
+    # os.remove(archivo + ".outi")
 
     res.append((promedio,proba))
 
